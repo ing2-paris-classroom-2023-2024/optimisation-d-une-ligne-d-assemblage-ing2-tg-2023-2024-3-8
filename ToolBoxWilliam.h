45d2b77dd0507c9983fe7a69abce0d6522d4c841
William Walker--Mercier <walkerwilly02@gmail.com>
2023-11-25 17:16:52 +0100

    Ajout de la structure sTache qui pour une tache comprend les informations suivantes: - id -couleur -temps

--- ToolBoxWilliam.h
+++ ToolBoxWilliam.h
@@ -8,4 +8,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct{
+    int id;
+    int col;
+    int temps;
+}sTache;
+
 #endif //OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_3_8_TOOLBOXWILLIAM_H
