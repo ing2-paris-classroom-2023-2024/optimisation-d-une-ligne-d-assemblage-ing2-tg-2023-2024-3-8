26af4b6f442e7282013a8905418b9dfdea1834d5
William Walker--Mercier <walkerwilly02@gmail.com>
2023-11-25 16:13:39 +0100

    Ajout du sous programme wReadFileTimeOperation, qui va lire operations.txt , et compter le nombre de sommet que l'on a . Par la suite, il devra remplir un tableau avec tous les sommets qui existent, et on réfléchit à un tableau de structure pour stocker les différentes informations correspondant à l'opération (on va regarder dans les autres branches)

--- main.c
+++ main.c
@@ -4,10 +4,43 @@
 
 #include "ToolBoxWilliam.h"
 
+int* wReadFileTimeOperation();
 
 int main(){
-    printf("Hello World");
+    wReadFileTimeOperation();
     return 0;
 }
 
-//Ceci est un commentaire
+int* wReadFileTimeOperation() {
+
+    //Declaration des variables
+    FILE *fFile;
+    int v;
+    int vNombreLignes = 0;
+    int v2 = '\0';
+
+
+
+    //Ouverture du fichier
+    fFile = fopen("operations.txt", "r");
+
+    //Vérification de l'ouverture du fichier
+    if (fFile == NULL) {
+        printf("File reading error");
+    }
+
+
+    while ((v = fgetc(fFile)) != EOF) {
+        if (v == '\n')
+            vNombreLignes++;
+        v2 = v;
+    }
+
+    /* Ici, v2 est égal au caractère juste avant le EOF. */
+    if (v2 != '\n')
+        vNombreLignes++; /* Dernière ligne non finie */
+    // Utilisez fseek pour déplacer le curseur au début du fichier
+    printf("Le nombre de lignes est de %d",vNombreLignes);
+    fseek(fFile, 0, SEEK_SET);
+    return 0;
+}
