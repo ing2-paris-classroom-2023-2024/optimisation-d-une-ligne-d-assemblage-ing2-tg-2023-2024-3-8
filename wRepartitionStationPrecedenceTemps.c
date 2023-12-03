206f980e39ac4b75a201dc97054e9b4e960245d4
William Walker--Mercier <walkerwilly02@gmail.com>
2023-12-03 15:14:30 +0100

    AJout de différentes fonctions: -getId qui renvoie l'id d'une tache à partir de l'index dans le tableau de taches -getIndex qui renvoie l'index d'une tache à partir de son ID dans le tableau -max qui renvoie la valeur maximale entre un entier a et un entier b -comparerDegree : une fonction permettant de comparer deux éléments du tableau. Cette fonction doit obligatoirement être prototypée ainsi : int (*compareFunction)( const void * ptr1, const void * ptr2 ). Cette fonction doit, de plus, renvoyer : une valeur < 0, si la valeur pointée par ptr1 est inférieure à celle pointée par ptr2 la valeur 0, si la valeur pointée par ptr1 est égale à celle pointée par ptr2 une valeur > 0, si la valeur pointée par ptr1 est supérieure à celle pointée par ptr2 (voir source koor.fr mis après le nom de la fonction) -tri par ordre topologique qui va trier par ordre de degré(nombre de predecesseur pour pouvoir faire l'action) croissant les taches permettant ainsi de savoir l'ordre

--- wRepartitionStationPrecedenceTemps.c
+++ wRepartitionStationPrecedenceTemps.c
@@ -1,43 +1,82 @@
-//
-// Created by Willyson on 02/12/2023.
-//
-
 #include "ToolBoxWilliam.h"
 
-void wTriTopologique(sTache *prListeTache) {
-    int n = prListeTache[0].id; // Nombre total de tâches
-    int *successeurs = calloc(n + 1, sizeof(int));
-    int *queue = calloc(n, sizeof(int)), front = 0, rear = 0;
 
-    // Calculer le nombre de successeurs pour chaque tâche
-    for (int i = 1; i <= n; i++) {
-        for (int j = 0; j < prListeTache[i].nbPrecedentes; j++) {
-            successeurs[prListeTache[i].tListeTachePrecedente[j]]++;
-        }
-    }
-
-    // Ajouter les tâches sans prédécesseurs à la file d'attente
-    for (int i = 1; i <= n; i++) {
-        if (successeurs[i] == 0) {
-            queue[rear++] = i;
-        }
-    }
-
-    // Appliquer le tri topologique
-    while (front < rear) {
-        int actuel = queue[front++];
-        printf("Tache %d\n", prListeTache[actuel].id);
-
-        for (int i = 0; i < prListeTache[actuel].nbPrecedentes; i++) {
-            int precedente = prListeTache[actuel].tListeTachePrecedente[i];
-            if (--successeurs[precedente] == 0) {
-                queue[rear++] = precedente;
-            }
-        }
-    }
-    free(successeurs);
-    free(queue);
+int getId(int index,sTache *prListeTache) {
+    return prListeTache[index].id;
 }
 
+// Renvoie l'index d'une tâche à partir de son ID
+int getIndex(int id,sTache * prListeTaches,int prNombreTaches) {
+    for(int i=0; i < prNombreTaches; i++) {
+        if(prListeTaches[i].id == id) {
+            return i;
+        }
+    }
+    return -1; // id non trouvé
+}
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
 
+// Fonction de comparaison pour qsort
+int comparerDegres(const void* t1, const void* t2) {
+    //https://koor.fr/C/cstdlib/qsort.wp
+    sTache* temp1 = (sTache*) t1;
+    sTache* temp2 = (sTache*) t2;
+    return (temp1->degreeEntree - temp2->degreeEntree);
+}
+
+int wCalculerDegreeEntree(sTache *prTache, sTache *prlisteTache, int nbTaches) {
+    int degre = 0;
+    prTache->marque = 1; // Marquer la tâche comme visitée
+
+    for (int i = 0; i < prTache->nbPredecesseur; i++) {
+        int vIdPrec = prTache->tListeTachePrecedente[i];
+        int indexPrec = getIndex(vIdPrec,prlisteTache,nbTaches);
+        sTache* sPrec = &prlisteTache[indexPrec];
+
+        if (sPrec->marque == 1) {
+            // Cycle détecté
+            printf("Cycle detecte sur tache %d\n", prTache->id);
+            exit(EXIT_FAILURE);
+        }
+
+        if (sPrec->degreeEntree == -1) {
+            // Récursion si non visitée
+            int deg_prec = wCalculerDegreeEntree(sPrec, prlisteTache, nbTaches);
+            degre = max(degre, deg_prec + 1);
+        } else {
+            // Sinon, incrémenter le degré
+            degre = max(degre, sPrec->degreeEntree + 1);
+        }
+    }
+
+    prTache->marque = 0; // Désactiver la marque
+    prTache->degreeEntree = degre;
+    return degre;
+}
+
+sTache* wTriTopologique(sTache* prListeTache, sParametre *prParametre) {
+    //https://perso.ens-lyon.fr/frederic.vivien/Enseignement/Algo-2001-2002/Corrige-TD09.pdf
+    //https://ressources.unisciel.fr/algoprog/s51gpcours/emodules/gp03mexerc1/res/gp03exerc1-enonce-cpp-xxx.pdf
+
+    for (int i = 0; i < prParametre->cNombreOperations; i++) {
+        prListeTache[i].degreeEntree = -1;
+        prListeTache[i].marque = 0;
+    }
+    for (int i = 0; i < prParametre->cNombreOperations; i++) {
+        if (prListeTache[i].degreeEntree == -1) {
+            prListeTache[i].degreeEntree = wCalculerDegreeEntree(&prListeTache[i], prListeTache, prParametre->cNombreOperations);
+        }
+    }
+    // Tri par degré croissant
+    qsort(&prListeTache[0], prParametre->cNombreOperations, sizeof(sTache), comparerDegres);
+
+    // Affichage des tâches triées
+    for (int i = 0; i < prParametre->cNombreOperations; i++) {
+        int id = getId(i,prListeTache);
+        printf("Tache %d (degre %d)\n", id, prListeTache[i].degreeEntree);
+    }
+    return prListeTache;
+}
 
